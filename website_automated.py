bb1b227fda65694ce67a37f87e5f3561efbdb528
Kevintzy <73383301+Kevintzy@users.noreply.github.com>
2025-01-13 01:31:15 +0800

    Update website_automated.py
    Updated checking function for connection variables

--- website_automated.py
+++ website_automated.py
@@ -26,6 +26,11 @@ def get_db_connection():
             database=DB_NAME,
             cursorclass=pymysql.cursors.DictCursor
         )
+        print("For checking...\n")
+        print(f"DB_HOST: {os.getenv('DB_HOST')}")
+        print(f"DB_USER: {os.getenv('DB_USER')}")
+        print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
+        print(f"DB_NAME: {os.getenv('DB_NAME')}")
         print("Database connection established.")
         return connection
     except pymysql.MySQLError as e:
